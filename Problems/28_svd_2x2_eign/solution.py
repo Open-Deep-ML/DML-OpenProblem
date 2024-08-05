37b0e84452da7278ca7f5436c3fc926826fe01b4
EpsIotaPi <1037272365@qq.com>
2024-08-05 20:06:48 +0800

    Restored the `svd_2x2`, and revised the `check_svd`, which will expect V already be transposed.

--- Problems/28_svd_2x2_eign/solution.py
+++ Problems/28_svd_2x2_eign/solution.py
@@ -2,21 +2,28 @@ import numpy as np
 
 
 def svd_2x2(A: np.ndarray) -> tuple:
-    AAt = np.dot(A, A.transpose())
+    y1, x1 = (A[1, 0] + A[0, 1]), (A[0, 0] - A[1, 1])
+    y2, x2 = (A[1, 0] - A[0, 1]), (A[0, 0] + A[1, 1])
 
-    eig_values, U = np.linalg.eig(AAt)
-    if eig_values[0] < eig_values[1]:
-        U[:, 0] += U[:, 1]
-        U[:, 1] = U[:, 0] - U[:, 1]
-        U[:, 0] -= U[:, 1]
-        eig_values[0] += eig_values[1]
-        eig_values[1] = eig_values[0] - eig_values[1]
-        eig_values[0] -= eig_values[1]
+    h1 = np.sqrt(y1 ** 2 + x1 ** 2)
+    h2 = np.sqrt(y2 ** 2 + x2 ** 2)
 
-    singular_values = np.sqrt(eig_values)
-    V = np.dot(np.dot(A.transpose(), U), np.diag(1 / singular_values))
+    t1 = x1 / h1
+    t2 = x2 / h2
 
-    return U, singular_values, V
+    cc = np.sqrt((1.0 + t1) * (1.0 + t2))
+    ss = np.sqrt((1.0 - t1) * (1.0 - t2))
+    cs = np.sqrt((1.0 + t1) * (1.0 - t2))
+    sc = np.sqrt((1.0 - t1) * (1.0 + t2))
+
+    c1, s1 = (cc - ss) / 2.0, (sc + cs) / 2.0
+    U = np.array([[-c1, -s1], [-s1, c1]])
+
+    s = np.array([(h1 + h2) / 2.0, abs(h1 - h2) / 2.0])
+
+    V = np.diag(1.0 / s) @ U.T @ A
+
+    return U, s, V
 
 
 def check_svd(U, s, V, A):
@@ -26,7 +33,7 @@ def check_svd(U, s, V, A):
     # SVD Condition:
     # 1. U*S*V.T == A
     # 2. U and V are orthogonal matrix -> U*U.T == E, V*V.T == E
-    result = U @ np.diag(s) @ V.T
+    result = U @ np.diag(s) @ V
     return is_equal(result, A) and is_equal(U @ U.T, np.eye(U.shape[0])) and is_equal(V @ V.T, np.eye(V.shape[0]))
 
 
@@ -45,7 +52,5 @@ def test_svd_2x2():
 
 
 if __name__ == "__main__":
-    A = np.array([[-10, 8], [10, -1]])
-
     test_svd_2x2()
     print("All svd_2x2 tests passed.")
