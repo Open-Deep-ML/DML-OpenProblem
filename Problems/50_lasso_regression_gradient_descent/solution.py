1b82def31d50d3ac21a26023994c27b9817e618f
Artem Frenk <afrenk@wpi.edu>
2024-09-06 16:38:37 -0400

    added second test case

--- Problems/50_lasso_regression_gradient_descent/solution.py
+++ Problems/50_lasso_regression_gradient_descent/solution.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def l1_regularization(X:np.array, y:np.array, alpha: float=0.1, learning_rate: float=0.01, max_iter: int=1000, tol: float=1e-4) -> tuple:
+def l1_regularization_gradient_descent(X:np.array, y:np.array, alpha: float=0.1, learning_rate: float=0.01, max_iter: int=1000, tol: float=1e-4) -> tuple:
     n_samples, n_features = X.shape
     # zero out weights and biases
     weights = np.zeros(n_features)
@@ -19,20 +19,27 @@ def l1_regularization(X:np.array, y:np.array, alpha: float=0.1, learning_rate: f
 
     return weights, bias
 
-def test_l1_regularization():
+def test_l1_regularization_gradient_descent():
 
     X = np.array([[0,0], [1, 1], [2, 2]])
     y = np.array([0, 1, 2])
     alpha = 0.1
-    l1_regularization(X,y , alpha = 0.1, max_iter = 5000)
-    expected_weights = np.array([0.4250, 0.4250])
-    expected_bias = 0.150
-    weights, bias = l1_regularization(X, y, alpha=alpha, learning_rate=0.01, max_iter=1000)
-
+    l1_regularization_gradient_descent(X,y , alpha = 0.1, max_iter = 5000)
+    expected_weights = np.array([0.425, 0.425])
+    expected_bias = 0.15
+    weights, bias = l1_regularization_gradient_descent(X, y, alpha=alpha, learning_rate=0.01, max_iter=1000)
     assert np.allclose(weights, expected_weights, atol=0.01), "Test case 1 failed"
     assert np.isclose(bias, expected_bias, atol=0.01), "Test case 1 failed"
 
 
+    X = np.array([[0,1], [1,2], [2,3], [3,4], [4,5]])
+    y = np.array([1, 2, 3, 4, 5])
+    alpha = 0.1
+    expected_weights = np.array([0.27, 0.68])
+    expected_bias = 0.4 
+    weights, bias = l1_regularization_gradient_descent(X, y, alpha=alpha, learning_rate=0.01, max_iter=1000)
+    assert np.allclose(weights, expected_weights, atol=0.01), "Test case 2 failed"
+    assert np.isclose(bias, expected_bias, atol=0.01), "Test case 2 failed"
 if __name__ == "__main__":
-    test_l1_regularization()
+    test_l1_regularization_gradient_descent()
     print("All l1_regularization tests passed.")
