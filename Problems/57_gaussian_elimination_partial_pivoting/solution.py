4b48247e0473e0ca1e8e315f7fb2bf645f1cb01f
Paddy Wardle <paddy.wardle@yahoo.com>
2024-10-06 19:42:17 +0100

    [Problem-57] changing expected to numerical values

--- Problems/57_gaussian_elimination_partial_pivoting/solution.py
+++ Problems/57_gaussian_elimination_partial_pivoting/solution.py
@@ -46,7 +46,7 @@ def test_gaussian_elimination():
     # Test case 1: basic test
     A_1 = np.array([[2,8,4],[2,5,1],[4,10,-1]], dtype=float)
     b_1 = np.array([2,5,1], dtype=float)
-    expected_1 = np.linalg.solve(A_1, b_1)
+    expected_1 = np.array([11., -4.,  3.])
     output_1 = gaussian_elimination(A_1, b_1)
     assert np.allclose(output_1, expected_1, atol=0.01), f"Test case 1 failed: expected {expected_1}, got {output_1}"
 
@@ -61,15 +61,14 @@ def test_gaussian_elimination():
         [0, 0, 0, 0, 1, 2, 11]
     ], dtype=float)
     b_2 = np.array([1, 2, 3, 4, 5, 6, 7], dtype=float)
-
-    expected_2 = np.linalg.solve(A_2,b_2)
+    expected_2 = np.array([-0.4894027, 0.36169985, 0.2766003, 0.25540569, 0.31898951, 0.40387497, 0.53393278])
     output_2 = gaussian_elimination(A_2, b_2)
     assert np.allclose(output_2, expected_2, atol=0.01), f"Test case 2 failed: expected {expected_2}, got {output_2}"
 
     # Test case 3: Multiple feature inputs
     A_3 = np.array([[2, 1, -1], [-3, -1, 2], [-2, 1, 2]], dtype=float)
     b_3 = np.array([8, -11, -3], dtype=float)
-    expected_3 = np.linalg.solve(A_3,b_3)
+    expected_3 = np.array([2., 3., -1.])
     output_3 = gaussian_elimination(A_3, b_3)
     assert np.allclose(output_3, expected_3, atol=0.01), f"Test case 3 failed: expected {expected_3}, got {output_3}"
 
