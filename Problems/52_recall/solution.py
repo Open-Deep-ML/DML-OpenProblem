e301d9bbc8297ff1818d9ed7918f874ec5f55a71
Tom Merkh <tom.merkh@q-ctrl.com>
2024-10-28 12:51:09 -0700

    Improve solution to recall

--- Problems/52_recall/solution.py
+++ Problems/52_recall/solution.py
@@ -1,13 +1,11 @@
 import numpy as np
 
 def recall(y_true, y_pred):
-    tp = np.sum((y_true == 1) & (y_pred == 1))
-    fn = np.sum((y_true == 1) & (y_pred == 0))
-
-    try:
-        return round(tp / (tp + fn), 3)
-    except ZeroDivisionError:
+    denom = sum(y_true)
+    if denom == 0:
         return 0.0
+    return round(y_pred.dot(y_true)/denom, 3)
+
 
 def test_recall():
     # Test case 1
