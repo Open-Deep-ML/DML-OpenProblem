b154b924ebe7dabba8ab62ff0104d8f75e9d9fd2
rittik9 <rittikpanda24@gmail.com>
2024-10-29 01:19:36 +0530

    feat: add test following sklearn official r_squared implementation

--- Problems/70_r-squared/solution.py
+++ Problems/70_r-squared/solution.py
@@ -26,7 +26,7 @@ def r_squared(y_true, y_pred):
     try:
         # Calculate R-squared
         r2 = 1 - (ssr / sst)
-        if np.isnan(r2):
+        if np.isnan(r2) or np.isinf(r2):
             return 0.0
         return round(r2, 3)
     except ZeroDivisionError:
@@ -57,18 +57,30 @@ def test_r_squared():
     y_pred = np.array([3, 3, 3, 3, 3])
     expected_output = 0.000
     assert r_squared(y_true, y_pred) == expected_output, "Test case 4 failed"
+    
+    # Test case 5
+    y_true = np.array([3, 3, 3, 3, 3])
+    y_pred = np.array([1, 2, 3, 4, 5])
+    expected_output = 0.000
+    assert r_squared(y_true, y_pred) == expected_output, "Test case 5 failed"
 
-    # Test case 5: Negative R-squared (predictions worse than mean)
+    # Test case 6: Negative R-squared (predictions worse than mean)
     y_true = np.array([1, 2, 3, 4, 5])
     y_pred = np.array([5, 4, 3, 2, 1])
     expected_output = -3.000
-    assert r_squared(y_true, y_pred) == expected_output, "Test case 5 failed"
+    assert r_squared(y_true, y_pred) == expected_output, "Test case 6 failed"
 
-    # Test case 6: All zeros (edge case)
+    # Test case 7: All zeros 
     y_true = np.array([0, 0, 0, 0, 0])
     y_pred = np.array([0, 0, 0, 0, 0])
     expected_output = 1.000
-    assert r_squared(y_true, y_pred) == expected_output, "Test case 6 failed"
+    assert r_squared(y_true, y_pred) == expected_output, "Test case 7 failed"
+    
+    # Test case 8 : output = -inf
+    y_true = np.array([-2, -2, -2])
+    y_pred = np.array([-2, -2, -2 + 1e-8])
+    expected_output = 0.000
+    assert r_squared(y_true, y_pred) == expected_output, "Test case 8 failed"
 
 if __name__ == "__main__":
     test_r_squared()
