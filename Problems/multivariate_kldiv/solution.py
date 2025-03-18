e47f5b7faf252db8e9fd65a592b40bb2ed625ee7
Turkunov Y. <55660526+turkunov@users.noreply.github.com>
2025-03-18 21:51:43 +0300

    Replaced linalg.trace with a custom func

--- Problems/multivariate_kldiv/solution.py
+++ Problems/multivariate_kldiv/solution.py
@@ -7,11 +7,15 @@ def calculate_params(dist1: np.ndarray, dist2: np.ndarray) -> tuple[np.ndarray,.
 
 def multivariate_kl_divergence(mu_p:np.ndarray, Cov_p:np.ndarray, 
                                mu_q:np.ndarray, Cov_q:np.ndarray) -> float:
+    
+    def trace(x: np.ndarray) -> float:
+        return np.diag(x).sum()
+    
     p = Cov_p.shape[0]
     return float(1/2 * (
         np.log(np.linalg.det(Cov_q)/np.linalg.det(Cov_p)) \
         - p + (mu_p-mu_q).T @ np.linalg.inv(Cov_q) @ (mu_p-mu_q) \
-        + np.linalg.trace(np.linalg.inv(Cov_q) @ Cov_p)
+        + trace(np.linalg.inv(Cov_q) @ Cov_p)
     ).round(5))
 
 
