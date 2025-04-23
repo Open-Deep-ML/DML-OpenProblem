534a2b5ead8755d163c5c8a749c62aa139660be6
silvermete0r <gregormelon.2003@gmail.com>
2025-04-23 10:02:14 +0500

    8-problem-added-singular-matrix-tests

--- Problems/8_Calculate_2x2_Matrix_Inverse/solution.py
+++ Problems/8_Calculate_2x2_Matrix_Inverse/solution.py
@@ -17,6 +17,14 @@ def test_inverse_2x2() -> None:
     matrix = [[2, 1], [6, 2]]
     assert inverse_2x2(matrix) == [[-1.0, 0.5], [3.0, -1.0]]
 
+    # Test case 3
+    matrix = [[3, 4], [6, 8]]
+    assert inverse_2x2(matrix) == None  # Singular matrix
+
+    # Test case 4
+    matrix = [[5, 2], [10, 4]]
+    assert inverse_2x2(matrix) == None  # Singular matrix
+
 if __name__ == "__main__":
     test_inverse_2x2()
     print("All inverse_2x2 tests passed.")
