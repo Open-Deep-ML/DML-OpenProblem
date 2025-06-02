7500ec6b5c453f720691ab2cd3131796c789e22e
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-06-02 00:59:13 -0400

    add asserts for adamax optimizer

--- Problems/X_adamax_optimizer/solution.py
+++ Problems/X_adamax_optimizer/solution.py
@@ -20,6 +20,13 @@ def adamax_optimizer(parameter, grad, m, u, t, learning_rate=0.002, beta1=0.9, b
     Returns:
         tuple: (updated_parameter, updated_m, updated_u)
     """
+    assert learning_rate > 0, "Learning rate must be positive"
+    assert 0 <= beta1 < 1, "Beta1 must be between 0 and 1"
+    assert 0 <= beta2 < 1, "Beta2 must be between 0 and 1"
+    assert epsilon > 0, "Epsilon must be positive"
+    assert all(m >= 0) if isinstance(m, np.ndarray) else m >= 0, "m must be non-negative"
+    assert all(u >= 0) if isinstance(u, np.ndarray) else u >= 0, "u must be non-negative"
+
     # Update biased first moment estimate
     m = beta1 * m + (1 - beta1) * grad
 
