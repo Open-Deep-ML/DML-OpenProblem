b549fda75786b9f8c2f47c3cd12d45c500d08b24
moe18 <chabotmoe1@gmail.com>
2025-07-06 09:39:10 -0400

    added a new question for n armed bandit

---
+++ questions/158_epsilon-greedy-action-selection-for-n-armed-bandit/starter_code.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def epsilon_greedy(Q, epsilon=0.1):
+    """
+    Selects an action using epsilon-greedy policy.
+    Q: np.ndarray of shape (n,) -- estimated action values
+    epsilon: float in [0, 1]
+    Returns: int, selected action index
+    """
+    # Your code here
+    pass
