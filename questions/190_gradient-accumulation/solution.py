812f5453f6884ebb43229eb4c7965169a20f8cfc
komaksym <kovalmaksym2@gmail.com>
2025-09-29 14:26:04 +0200

    add solution

--- questions/190_gradient-accumulation/solution.py
+++ questions/190_gradient-accumulation/solution.py
@@ -1,3 +1,13 @@
-def your_function(...):
-    # reference implementation
-    ...
+import numpy as np
+
+def accumulate_gradients(grad_list):
+    """
+    Accumulates (sums) a list of gradient arrays into a single array.
+
+    Args:
+        grad_list (list of np.ndarray): List of gradient arrays, all of the same shape.
+
+    Returns:
+        np.ndarray: The accumulated (summed) gradients, same shape as input arrays.
+    """
+    return np.sum(grad_list, axis=0).astype(float)
