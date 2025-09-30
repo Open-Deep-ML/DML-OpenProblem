e7c4ac50644cf232a6e60b4e5f66c31dfb5a32c9
komaksym <kovalmaksym2@gmail.com>
2025-09-30 14:18:37 +0200

    add starter code

--- questions/195_gradient-checkpointing/starter_code.py
+++ questions/195_gradient-checkpointing/starter_code.py
@@ -1,4 +1,15 @@
-# Implement your function below.
+import numpy as np
 
-def your_function(...):
+# Implement your function below.
+def checkpoint_forward(funcs, input_arr):
+    """
+    Applies a list of functions in sequence to the input array, simulating gradient checkpointing by not storing intermediates.
+
+    Args:
+        funcs (list of callables): List of functions to apply in sequence.
+        input_arr (np.ndarray): Input numpy array.
+
+    Returns:
+        np.ndarray: The output after applying all functions, same shape as output of last function.
+    """
     pass
