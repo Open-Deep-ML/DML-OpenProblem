e54af2b1513e6048dbc1fbd16040a1cf3bb3c5c0
komaksym <kovalmaksym2@gmail.com>
2025-09-30 14:18:26 +0200

    add solution

--- questions/195_gradient-checkpointing/solution.py
+++ questions/195_gradient-checkpointing/solution.py
@@ -1,3 +1,17 @@
-def your_function(...):
-    # reference implementation
-    ...
+import numpy as np
+
+def checkpoint_forward(funcs, input_arr):
+    """
+    Applies a list of functions in sequence to the input array, simulating gradient checkpointing by not storing intermediates.
+
+    Args:
+        funcs (list of callables): List of functions to apply in sequence.
+        input_arr (np.ndarray): Input numpy array.
+
+    Returns:
+        np.ndarray: The output after applying all functions, same shape as output of last function.
+    """
+    x = input_arr
+    for f in funcs:
+        x = f(x)
+    return x.astype(float)
