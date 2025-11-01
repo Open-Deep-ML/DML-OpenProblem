67f2ee2c82ed9e525e2d767eb1a8dcf1042faecd
dsaha21 <sahadripto21@gmail.com>
2025-11-01 13:23:09 +0530

    add: All required files for iou-score

--- questions/_template/starter_code.py
+++ questions/_template/starter_code.py
@@ -1,4 +1,12 @@
 # Implement your function below.
 
-def your_function(...):
-    pass
+def iou_score(bboxA, bboxB):
+    #xA = np.maximum(bboxA[0], bboxB[0])
+
+    #interWidth = np.maximum(0, xB - xA + 1)
+    #interArea = interWidth * interHeight
+
+    #find bbox areas of A and B
+
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    return round(iou, 3)
